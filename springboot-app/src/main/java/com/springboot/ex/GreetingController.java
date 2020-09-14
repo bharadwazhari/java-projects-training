48120b7a514c640186d425563ecc9dd299c7b5b5
BHARADWAZ HARI <bharadwaz@centrifugecorp.com>
2020-09-13 22:37:47 -0400

    simple-training

--- springboot-app/src/main/java/com/springboot/ex/GreetingController.java
+++ springboot-app/src/main/java/com/springboot/ex/GreetingController.java
@@ -2,14 +2,12 @@ package com.springboot.ex;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import com.springboot.dao.EmployeeDao;
 import com.springboot.service.EmployeeService;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class GreetingController {
@@ -20,6 +18,9 @@ public class GreetingController {
     @Autowired
     private EmployeeService service;
 
+    @Autowired
+    private EmployeeDao dao1;
+
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
@@ -29,5 +30,10 @@ public class GreetingController {
     public Employee getEmployeeDetails(@PathVariable("id") long employeeId) {
         Employee e = service.getEmployeeDetails(employeeId);
         return e;
+    }
+
+    @PostMapping("/employeedetails")
+    public Employee getEmployeeDetails(Employee emp) {
+        return emp;
     }
 }
